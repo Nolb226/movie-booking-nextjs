e0ee31ddddac8de7c2e3f1a9ccbcb7586f97703e
NTKH <hn26677@gmail.com>
2024-10-21 08:27:08 +0700

    feat:env detect when missing

---
+++ env.mjs
@@ -0,0 +1,11 @@
+import { createEnv } from '@t3-oss/env-nextjs'
+import { z } from 'zod'
+
+export const env = createEnv({
+    client: {
+        MOVIE_API_URL: z.string().url().min(1),
+    },
+    runtimeEnv: {
+        MOVIE_API_URL: process.env.MOVIE_API_URL,
+    },
+})
